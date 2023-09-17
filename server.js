24ac2c525832110fa138767bc0029725f6fe947a
Ashwitha Lingannagari <ashwitharao51@gmail.com>
2023-09-17 16:59:20 -0400

    Added server, Axios, and ChartJS

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const fileSystem = require('fs');
 const app = express();
 const port = 3000;
 const importJSON = fileSystem.readFileSync('budget.json', 'utf8');
+const importJSON1 = fileSystem.readFileSync('data.json', 'utf8');
 const budget = JSON.parse(importJSON);
+const data = JSON.parse(importJSON1);
 
 app.use('/', express.static('public'));
 
@@ -15,6 +17,10 @@ app.get('/hello', (req, res) => {
 
 app.get('/budget', (req, res) => {
     res.json(budget);
+}); 
+
+app.get('/data', (req, res) => {
+    res.json(data);
 }); 
 
 
