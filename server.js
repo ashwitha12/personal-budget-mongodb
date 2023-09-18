7283c517d93768c0ef3e24ba98d5c8e98512f581
Ashwitha Lingannagari <ashwitharao51@gmail.com>
2023-09-17 20:12:54 -0400

    Added D3JS chart

--- server.js
+++ server.js
@@ -2,28 +2,22 @@ const express = require('express');
 const fileSystem = require('fs');
 const app = express();
 const port = 3000;
-const importJSON = fileSystem.readFileSync('budget.json', 'utf8');
-const importJSON1 = fileSystem.readFileSync('data.json', 'utf8');
-const budget = JSON.parse(importJSON);
-const data = JSON.parse(importJSON1);
+const importJSON = fileSystem.readFileSync('data.json', 'utf8');
+const budgetData = JSON.parse(importJSON);
 
-app.use('/', express.static('public'));
+app.get('/hello', (req, res) => 
+{
+res.send('Hello World!');
+});
 
-
-
-app.get('/hello', (req, res) => {
-    res.send('Hello World!');
-}); 
+app.listen(port, () =>
+{
+console.log(`Example app listening at http://localhost:${port}`);
+}
+);
 
 app.get('/budget', (req, res) => {
-    res.json(budget);
-}); 
-
-app.get('/data', (req, res) => {
-    res.json(data);
-}); 
-
-
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    res.json(budgetData);
 });
+
+app.use('/',express.static('public'));
