aeeb046d9ba647d6bf19987d6d1256307ee2613c
Ashwitha Lingannagari <ashwitharao51@gmail.com>
2023-10-15 21:40:44 -0400

    quiz 8

--- models/budget_schema.js
+++ models/budget_schema.js
@@ -4,6 +4,7 @@ const  budgetSchema = new mongoose.Schema({
     title: {
         type: String,
         required: true,
+        
     },
     budget: {
         type: Number,
--- server.js
+++ server.js
@@ -39,7 +39,7 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
 })
 });
 
-app.post("/insertBudget", (req, res) => {
+app.post('/Budget', (req, res) => {
 
     mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
         .then(() => {
